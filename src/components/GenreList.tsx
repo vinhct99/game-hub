4174b1cbab9f8f126af557fdb7e936b5fc1de714
vinhct99 <90665073+vinhct99@users.noreply.github.com>
2023-09-21 10:23:12 +0700

    Filter games by genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,9 +1,20 @@
-import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
+import {
+  Button,
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 import { useState } from "react";
 
-const GenreList = () => {
+interface Props {
+  onSelectGenre: (genre: Genre) => void;
+}
+const GenreList = ({ onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
   if (error) return <Text>wrong</Text>;
   if (isLoading) return <Spinner />;
@@ -17,7 +28,13 @@ const GenreList = () => {
               borderRadius={8}
               src={getCroppedImageUrl(genre.image_background)}
             />
-            <Text fontSize="lg">{genre.name}</Text>
+            <Button
+              onClick={() => onSelectGenre(genre)}
+              fontSize="lg"
+              variant="link"
+            >
+              {genre.name}
+            </Button>
           </HStack>
         </ListItem>
       ))}
--- src/hooks/UseGames.ts
+++ src/hooks/UseGames.ts
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 import useData from "./useData";
+import { Genre } from "./useGenres";
 export interface Platform{
     id:number;
     name:string;
@@ -16,5 +17,5 @@ export interface Game {
 
   }
 
-const useGames = () => useData<Game>('/games');
+const useGames = (selectedGenre:Genre|null) => useData<Game>('/games',{params: {genres: selectedGenre?.id}},[selectedGenre?.id]);
 export default useGames;
