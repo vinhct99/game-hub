c9553479dea9dda0d45c96101cc5d5c3216c739d
vinhct99 <90665073+vinhct99@users.noreply.github.com>
2023-09-22 22:18:40 +0700

    Customize the theme to get dark

--- src/components/theme.tsx
+++ src/components/theme.tsx
@@ -3,5 +3,21 @@ import { extendTheme, ThemeConfig } from "@chakra-ui/react";
 const config: ThemeConfig = {
   initialColorMode: "dark",
 };
-const theme = extendTheme({ config });
+const theme = extendTheme({
+  config,
+  colors: {
+    gray: {
+      50: "#f9f9f9",
+      100: "#ededed",
+      200: "#d3d3d3",
+      300: "#b3b3b3",
+      400: "#a0a0a0",
+      500: "#676767",
+      600: "#6c6c6c",
+      700: "#303030",
+      800: "#121212",
+      900: "#111",
+    },
+  },
+});
 export default theme;
